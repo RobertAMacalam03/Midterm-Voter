c6a9101bed90d23d771991e9fa2330e04b00eaec
Robert <robertmacalam0603@gmail.com>
2025-05-16 19:51:15 +0800

    5th Commit

--- test_voter_checker.py
+++ test_voter_checker.py
@@ -5,16 +5,16 @@ from voter_checker import is_eligible_to_vote
 class TestVoterEligibility(unittest.TestCase):
 
     def test_underage_voter(self):
-        self.assertTrue(is_eligible_to_vote(17, True))
+        self.assertTrue(is_eligible_to_vote(16, True))
 
     def test_non_citizen_voter(self):
-        self.assertTrue(is_eligible_to_vote(28, False))
+        self.assertFalse(is_eligible_to_vote(18, False))
 
     def test_underage_non_citizen(self):
-        self.assertTrue(is_eligible_to_vote(15, False))
+        self.assertFalse(is_eligible_to_vote(15, False))
 
     def test_exactly_18_and_none_citizen(self):
-        self.assertTrue(is_eligible_to_vote(18, False))
+        self.assertFalse(is_eligible_to_vote(18, False))
 
     def test_negative_age(self):
-        self.assertTrue(is_eligible_to_vote(-20, True))
+        self.assertThrows(ValueError, is_eligible_to_vote, -1, True )
