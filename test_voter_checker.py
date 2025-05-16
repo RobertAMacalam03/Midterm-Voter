a6745dc1c0f11b4bf50462c297253bbc7fa57aa8
Robert <robertmacalam0603@gmail.com>
2025-05-16 20:10:09 +0800

    9th coomit

--- test_voter_checker.py
+++ test_voter_checker.py
@@ -17,4 +17,4 @@ class TestVoterEligibility(unittest.TestCase):
         self.assertFalse(is_eligible_to_vote(18, False))
 
     def test_negative_age(self):
-        self.assertRaises(is_eligible_to_vote(-30, False))
+        self.assertRaises(is_eligible_to_vote(-30, True))
